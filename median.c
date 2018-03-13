ed6d0a43d58a59e527ca6e995fbb4ce3756a6913
sgandt <35653041+sgandt@users.noreply.github.com>
2018-03-13 16:21:12 +0530

    Rename median element .c to median.c

---
+++ median.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,sum=0,i,a[120];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+printf("median is %d",sum/n;
+}
